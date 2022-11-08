2bab6c259472f6571b6949ecf68f2b8d7dd35ee3
haengbit <biit4894@sookmyung.ac.kr>
2022-11-08 17:15:41 +0900

    10주차_실습6

--- main.c
+++ main.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	FILE* fp;
-	int i = 0;
+	FILE* fp = NULL;
+//	char c;
 	char str[30];
-
-	fp = fopen("sample.txt", "w");
 	
-	while (i < 3) {
-		printf("input a word : ");
-		scanf("%s", str);
-		fprintf(fp, "%s\n", str);
-		i++;
-	}
+	fp = fopen("sample.txt", "r");
+	
+//	c = fgetc(fp);
+//	while ( (c = fgetc(fp)) != EOF) 
+//		putchar(c);
+	
+	
+	while ( fgets(str, fp) != NULL )
+		printf("%s", str);
 
 	fclose(fp);
 	
