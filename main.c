af1d3994b09965ed5ed54e5ed047bdc01b1b7d28
haengbit <biit4894@sookmyung.ac.kr>
2022-11-08 17:04:10 +0900

    10주차_실습5

--- main.c
+++ main.c
@@ -3,9 +3,21 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void main(int argc, char *argv[]) {
-	char str[30] = "happy C programming";
-	
-	printf("length of string \"%s\": %i", str, strlen(str));
+int main(int argc, char *argv[]) {
+	FILE* fp;
+	int i = 0;
+	char str[30];
 
+	fp = fopen("sample.txt", "w");
+	
+	while (i < 3) {
+		printf("input a word : ");
+		scanf("%s", str);
+		fprintf(fp, "%s\n", str);
+		i++;
+	}
+
+	fclose(fp);
+	
+	return 0;
 }
